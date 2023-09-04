11e3aeb5a2f822a87973a750ab0eb285fb4f7f05
Sultan Tlbkv <katana.st@yandex.com>
2023-09-04 10:43:04 +0600

    add null=True for expiration_date

--- medicine/models.py
+++ medicine/models.py
@@ -6,7 +6,7 @@ class Medicine(models.Model):
     price = models.DecimalField('Цена со скидкой', max_digits=10, decimal_places=2,  default=0)
     measure = models.CharField('Ед. измерения', max_length=10, default='')
     manufacturer = models.CharField('Производитель', max_length=300, default='')
-    expiration_date = models.DateField('Срок годности', default=None)
+    expiration_date = models.DateField('Срок годности', default=None, null=True)
 
     def __str__(self) -> str:
         return f'{self.name}-->{self.price}'
