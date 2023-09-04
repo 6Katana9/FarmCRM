4ab3ac2080faaebfbb2889128770957523698ca2
Sultan Tlbkv <katana.st@yandex.com>
2023-09-04 11:45:59 +0600

    .

--- medicine/views.py
+++ medicine/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.filters import SearchFilter
 from .models import Medicine
 from .serializers import MedicineSerializer
 
@@ -8,4 +8,5 @@ from .serializers import MedicineSerializer
 class MedicineApiView(ModelViewSet):
     queryset = Medicine.objects.all()
     serializer_class = MedicineSerializer
-
+    filter_backends = [SearchFilter]
+    search_fields = ['name']
